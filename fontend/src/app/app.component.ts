a789e2d94518cb9ba71e35762430ae12e835f202
Mario-Coxe <cox3site59@gmail.com>
2024-01-14 00:01:11 +0100

    feat(angular-material): instaled angular material

--- fontend/src/app/app.component.ts
+++ fontend/src/app/app.component.ts
@@ -6,13 +6,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
-  styles: [],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'fontend';
+  
 }
